c414cfa39ea3e5779e9db9361f9a76a81ee036ac
thachpholly <b1204051@gmail.com>
2016-09-22 21:37:10 +0700

    new respotery

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textReceive = new System.Windows.Forms.RichTextBox();
+            this.txtSend = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textReceive
+            // 
+            this.textReceive.Location = new System.Drawing.Point(24, 127);
+            this.textReceive.Name = "textReceive";
+            this.textReceive.Size = new System.Drawing.Size(657, 244);
+            this.textReceive.TabIndex = 0;
+            this.textReceive.Text = "";
+            this.textReceive.VScroll += new System.EventHandler(this.textReceive_VScroll);
+            this.textReceive.TextChanged += new System.EventHandler(this.text_TextChanged);
+            // 
+            // txtSend
+            // 
+            this.txtSend.Location = new System.Drawing.Point(135, 49);
+            this.txtSend.Name = "txtSend";
+            this.txtSend.Size = new System.Drawing.Size(267, 20);
+            this.txtSend.TabIndex = 1;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(408, 49);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 2;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(135, 75);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(84, 20);
+            this.txtIP.TabIndex = 3;
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(135, 101);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(84, 20);
+            this.txtPort.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(256, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Demo Host connect raspberry";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(52, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Command";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(52, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Rasp IP";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(52, 108);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Rasp Port";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(693, 383);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtSend);
+            this.Controls.Add(this.textReceive);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox textReceive;
+        private System.Windows.Forms.TextBox txtSend;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+
+    public partial class Form1 : Form
+    {
+        IPAddress address = IPAddress.Parse("192.168.137.1");
+        private const int PORT_NUMBER = 3129;
+        private const string RASP_IP = "192.168.137.48";
+        Thread childThread;
+
+        public Form1()
+        {
+            InitializeComponent();
+            txtIP.Text = RASP_IP;
+            txtPort.Text = PORT_NUMBER + "";
+            //iep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 3129);
+            try
+            {
+                ThreadStart childref = new ThreadStart(Server);
+                Console.WriteLine("Trong Main Thread: tao Thread con.");
+                childThread = new Thread(childref);
+                childThread.Start();
+            }
+
+            catch (ThreadAbortException e)
+            {
+                Console.WriteLine("Thread Abort Exception!!!");
+            }
+            finally
+            {
+                Console.WriteLine("Khong the bat Thread Exception!!!");
+            }
+            
+        }
+
+        void Server()
+        {
+            TcpListener listener = new TcpListener(address, PORT_NUMBER);
+
+            // 1. listen
+            listener.Start();
+            //Console.WriteLine("Server started on " + listener.LocalEndpoint);
+            //Console.WriteLine("Waiting for a connection...");
+            textReceive.Text += "Server started on ";
+
+            while (true)
+            {
+                Socket socket = listener.AcceptSocket();
+                Console.WriteLine("Connection received from " + socket.RemoteEndPoint);
+
+                var stream = new NetworkStream(socket);
+                var reader = new StreamReader(stream);
+                // 2. receive
+                string str = reader.ReadLine();
+
+                textReceive.Text =  textReceive.Text + "\n[" + DateTime.Now.ToString() + "]Reveived:" + str;
+
+                // 3. send
+                 //writer.WriteLine("Hello " + str);
+            }
+        }
+
+        private void text_TextChanged(object sender, EventArgs e)
+        {
+            textReceive.SelectionStart = textReceive.Text.Length;
+            textReceive.ScrollToCaret();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            TcpClient client = new TcpClient();
+
+            // 1. connect
+            client.Connect(txtIP.Text, Convert.ToInt32(txtPort.Text));
+            Stream stream = client.GetStream();
+            var writer = new StreamWriter(stream);
+            writer.AutoFlush = true;
+            //Console.Write(s)
+            // 2. send
+            writer.WriteLine(txtSend.Text);
+            stream.Close();
+            client.Close();
+            MessageBox.Show(" xong!");
+        }
+
+        private void textReceive_VScroll(object sender, EventArgs e)
+        {
+           // txtSend.Text = "ffs";
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            //childThread.Abort();
+
+            Application.Exit();
+        }
+    }
+}
